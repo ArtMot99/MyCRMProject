a904c43f1dd2309d6d09602ef59c532dbb07add6
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-20 00:04:43 +0200

    Added comment

--- crmproject/apps.py
+++ crmproject/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class CrmprojectConfig(AppConfig):
+    """
+    Base settings for admin panel
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crmproject'

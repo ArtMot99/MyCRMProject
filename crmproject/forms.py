fac3bd25b65f455c3c8909c545529fdd734db8a7
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-20 00:09:30 +0200

    Update forms.py and added form for Update company

--- crmproject/forms.py
+++ crmproject/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.core.exceptions import ValidationError
+
+from crmproject.models import Company
 from users.models import User
 
 
 class FormUpdateProfile(forms.ModelForm):
+    """
+    Form for Update profile settings
+    """
     username = forms.CharField(error_messages={'required': ''})
 
     class Meta:
@@ -23,3 +28,13 @@ class FormUpdateProfile(forms.ModelForm):
         return new_last_name
 
 
+class FormUpdateCompany(forms.ModelForm):
+    """
+    Form for Update company settings
+    """
+    update_at = forms.DateField(required=True)
+
+    class Meta:
+        model = Company
+        fields = ['name_of_company', 'director_name', 'director_surname',
+                  'location', 'about_company', 'photo', 'update_at']

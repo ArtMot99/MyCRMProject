9a1772996c48ee4f51cb4d11da2579a9c85fb7fb
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-19 09:03:45 +0200

    Added views for profile and update profile.

--- crmproject/views.py
+++ crmproject/views.py
@@ -1,9 +1,12 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView
 from django.views.generic.detail import SingleObjectMixin
 
+from crmproject.forms import FormUpdateProfile
 from crmproject.models import Company
+from users.models import User
 
 
 class AllCompanyView(LoginRequiredMixin, ListView):
@@ -25,3 +28,30 @@ class InfoAboutCompany(LoginRequiredMixin, SingleObjectMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['company'] = self.object
         return context
+
+
+class MyProfileView(LoginRequiredMixin, SingleObjectMixin, ListView):
+    model = User
+    template_name = 'crmproject/profile.html'
+    paginate_by = 5
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=User.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['users'] = self.object
+        return context
+
+
+class UpdateProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'crmproject/update_profile.html'
+    model = User
+    form_class = FormUpdateProfile
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.save()
+        return super().form_valid(form)

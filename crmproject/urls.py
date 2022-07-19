7c3a166d6e54783522ec554d7c2b7b27ebcdc805
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-19 09:03:16 +0200

    Added urls for profile and update profile.

--- crmproject/urls.py
+++ crmproject/urls.py
@@ -4,4 +4,6 @@ from crmproject import views
 urlpatterns = [
     path('', views.AllCompanyView.as_view(), name='index'),
     path('info/<int:pk>', views.InfoAboutCompany.as_view(), name='info'),
+    path('profile/<int:pk>', views.MyProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/update', views.UpdateProfileView.as_view(), name='update_profile'),
 ]

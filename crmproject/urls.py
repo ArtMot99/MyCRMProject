50765dc2133cafcc34d48c64a07cfe4afdf89be1
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-20 00:08:54 +0200

    Create structure for urls

--- crmproject/urls.py
+++ crmproject/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path, include
 from crmproject import views
 
+
+profile_urls = [
+    path('', views.MyProfileView.as_view(), name='profile'),
+    path('update/', views.UpdateProfileView.as_view(), name='update_profile'),
+    path('delete/', views.DeleteProfileView.as_view(), name='delete_profile'),
+]
+
+
+company_urls = [
+    path('', views.InfoAboutCompany.as_view(), name='info'),
+    path('update/', views.UpdateCompany.as_view(), name='update_company'),
+    path('delete/', views.DeleteCompanyView.as_view(), name='delete_company'),
+]
+
+
 urlpatterns = [
     path('', views.AllCompanyView.as_view(), name='index'),
-    path('info/<int:pk>', views.InfoAboutCompany.as_view(), name='info'),
-    path('profile/<int:pk>', views.MyProfileView.as_view(), name='profile'),
-    path('profile/<int:pk>/update', views.UpdateProfileView.as_view(), name='update_profile'),
+    path('info/<int:pk>', include(company_urls)),
+    path('profile/<int:pk>', include(profile_urls)),
 ]

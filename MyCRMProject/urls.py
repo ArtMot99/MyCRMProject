4368591dee8c943cc2c49d8422d586b58deedc60
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-21 15:38:32 +0200

    Removed error favicon.

--- MyCRMProject/urls.py
+++ MyCRMProject/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('main/', include('crmproject.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

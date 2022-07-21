c8df57aa3ccf0d2301a195954b645f7f16a72fa8
Artem <104223700+ArtMot99@users.noreply.github.com>
2022-07-21 15:38:54 +0200

    Create new view for projects.

--- crmproject/views.py
+++ crmproject/views.py
@@ -6,7 +6,7 @@ from django.views.generic.detail import SingleObjectMixin
 
 from crmproject.forms import FormUpdateProfile, FormUpdateCompany, CreateCompanyForm, CompanyPhoneFormSet, \
     CompanyEmailFormSet, CompanyPhoneForUpdate, CompanyEmailForUpdate
-from crmproject.models import Company
+from crmproject.models import Company, Project
 from users.models import User
 
 
@@ -171,4 +171,22 @@ class DeleteCompanyView(DeleteView):
     model = Company
     success_url = reverse_lazy('index')
     template_name = 'crmproject/company_delete.html'
+
+
+class ProjectInfoView(LoginRequiredMixin, SingleObjectMixin, ListView):
+    """
+    View for view detailed information about the projects
+    """
+    model = Project
+    template_name = 'crmproject/info_about_project.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Project.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project'] = self.object
+        return context
+
 
